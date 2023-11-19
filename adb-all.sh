4d0da0bdc35ee92a194ab207cb1a07eed1a69595
Zmzii <33648579+Zmzii@users.noreply.github.com>
2023-11-19 19:35:57 +0300

    Create adb-all.sh

---
+++ adb-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Connect to all devices from ip list
+if [ $# -eq 0 ]
+then
+  echo "Usage: ./adb-all.sh \"Adb shell Command(s)\""
+  exit 1
+fi
+
+DEVICES=`adb devices | tail -n +2 | cut -f1`
+for DEVICE in $DEVICES
+do
+  RUN="adb -s $DEVICE shell $2 &"
+  echo ">> $RUN"
+  ${RUN}
+done
