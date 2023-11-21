6e6e5adc976f1310113a92c74c246fff46b9e1f6
Zmzii <33648579+Zmzii@users.noreply.github.com>
2023-11-21 13:00:34 +0300

    Update adb-connect.sh

--- adb-connect.sh
+++ adb-connect.sh
@@ -13,18 +13,3 @@ while read -r ip
 do
   adb connect $ip:5555 &
 done < $1
-
-# ADB All[Send adb command to all connected devices]
-
-if [ -z $2 ]
-then
-  exit 1
-fi
-
-DEVICES=`adb devices | tail -n +2 | cut -f1`
-for DEVICE in $DEVICES
-do
-  RUN="adb -s $DEVICE shell $2 &"
-  echo $RUN
-  ${RUN}
-done
