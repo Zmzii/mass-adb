68e028d6c73319264d83f1330a0e045136e0936f
Zmzii <33648579+Zmzii@users.noreply.github.com>
2023-11-23 12:08:48 +0300

    Update adb-all.sh

--- adb-all.sh
+++ adb-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Connect to all devices from ip list
 if [ $# -eq 0 ]
 then
   echo "Usage: ./adb-all.sh \"Adb Command(s)\""
@@ -9,7 +10,7 @@ fi
 DEVICES=`adb devices | tail -n +2 | cut -f1`
 for DEVICE in $DEVICES
 do
-  RUN="adb -s $DEVICE shell $@ &"
+  RUN="adb -s $DEVICE $@ &"
   echo ">> $RUN"
   ${RUN}
 done
